29c0c5ffccdd4e3f2cd25330289da607827c7954
Shubham-391 <subhamy1234@gmail.com>
2023-09-12 15:28:21 +0530

    space, navbar, height

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -5,12 +5,20 @@ let services = document.querySelector(".services");
 let projects = document.querySelector(".projects");
 let about = document.querySelector(".about");
 let navbutton = document.querySelector(".nav-btn");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
 icon.addEventListener("click", function () {
     if (page.classList !== document.querySelector("right-0")) {
         page.classList.toggle("right-0");
         document.querySelector("body").classList.toggle("overflow-hidden");
+        span1.classList.toggle("close-icon1");
+        span2.classList.toggle("close-icon2");
+        span3.classList.toggle("close-icon3");
     }
 })
+
+
 
 home.addEventListener("click", function () {
     page.classList.toggle("right-0");
